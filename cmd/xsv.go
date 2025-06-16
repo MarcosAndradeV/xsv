ee272f2cc69b8bc0bf61b4cf30370d8066b31c43
MarcosAndradeV <mastermarcos1212@hotmail.com>
2025-06-16 12:20:46 -0300

    Init

---
+++ cmd/xsv.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+
+}
+
+type XSV struct {
+	Sep string
+	HeaderTypes []string
+	Header []string
+	Values []string
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MarcosAndradeV/xsv
+
+go 1.24.4
